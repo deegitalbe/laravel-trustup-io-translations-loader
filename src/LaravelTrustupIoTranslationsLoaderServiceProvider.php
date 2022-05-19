23ffbdb7452d147d2d5680bb5a3f20e7afbeb261
Florian Husquinet <florian.husquinet@deegital.be>
2022-05-19 13:55:02 +0200

    🐛

--- src/LaravelTrustupIoTranslationsLoaderServiceProvider.php
+++ src/LaravelTrustupIoTranslationsLoaderServiceProvider.php
@@ -18,9 +18,9 @@ class LaravelTrustupIoTranslationsLoaderServiceProvider extends PackageServicePr
         $package
             ->name('laravel-trustup-io-translations-loader')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel-trustup-io-translations-loader_table')
-            ->hasCommand(LaravelTrustupIoTranslationsLoaderCommand::class)
+            // ->hasViews()
+            // ->hasMigration('create_laravel-trustup-io-translations-loader_table')
+            // ->hasCommand(LaravelTrustupIoTranslationsLoaderCommand::class)
             ->hasRoute('webhooks');
     }
 }
