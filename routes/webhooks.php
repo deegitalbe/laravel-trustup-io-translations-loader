adbc3d766414eb22313c47445c17f442376afb8a
Florian Husquinet <florian.husquinet@deegital.be>
2022-05-19 15:34:40 +0200

    🚧

--- routes/webhooks.php
+++ routes/webhooks.php
@@ -4,5 +4,10 @@ use Deegitalbe\LaravelTrustupIoTranslationsLoader\LaravelTrustupIoTranslations;
 use Illuminate\Support\Facades\Route;
 
 Route::get('webhooks/laravel-trustup-io-translations/refresh', function () {
+    $authorizationKey = request()->header('X-Server-Authorization');
+    if ( ! $authorizationKey || $authorizationKey !== env('TRUSTUP_SERVER_AUTHORIZATION') ) {
+        return response('Invalid authorization key.', 401);
+    }
+    
     app(LaravelTrustupIoTranslations::class)->refresh();
 });
